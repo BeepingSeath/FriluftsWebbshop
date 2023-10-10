fb5c218781daaae3ad0830fecc6dccbd9235d64c
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-10-10 10:35:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>
+            JOYCAMP
+          </h1>
+        </nav>
+        <div className='hero'>
+          <div className='callToAction'>
+            <div className='filter'></div>
+            <h1>VÄLKOMMEN</h1>
+            <h2>Kolla på årets höstsortiment!</h2>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default App
