00d4187cc8a096f996cb224b4c3a706e86e659c9
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-10-19 14:44:22 +0200

    DatabeConnected

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,26 +1,27 @@
 import './card.css'
 
-function Card() {
+function Card(product) {
 
-
-
-    let Name = "Kiikii Original"
-    let Price = "699"
-    let Categories = "Comfy, Shirt"
-    let Colors = "Green, Blue, Black, White"
-    let image = "https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
+    console.log(product)
+    const item = product.product;
+    // let Name = "Kiikii Original"
+    // let Price = "699"
+    // let Categories = "Comfy, Shirt"
+    // let Colors = "Green, Blue, Black, White"
+    // let image = "https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1964&q=80"
     let imageAlt = "An Extremely Handsome Gorilla"
 
     return (
-        <>
-            <div className='cardBox'>
-                <h2>{Name}</h2>
-                <img src={image} alt={imageAlt} className='cardImg' />
+        <div className='cardBox'>
+            <h2>{item.name}</h2>
+            <img src={item.image} alt={imageAlt} className='cardImg' />
+            <h3>{item.price} :-</h3>
+            <h4>Sizes: {item.sizes}</h4>
+            {/* <img src={image} alt={imageAlt} className='cardImg' />
                 <h3>{Price} :-</h3>
                 <h4>{Categories}</h4>
-                <h4>{Colors}</h4>
-            </div>
-        </>
+                <h4>{Colors}</h4> */}
+        </div>
     )
 }
 
